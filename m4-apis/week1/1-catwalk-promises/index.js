6c81a39f207f4ca58921f64b1b0fdff49a5d3efd
Muatasim-Aswad <malasoad2021@gmail.com>
2024-08-30 18:58:19 +0200

    add apis week1

---
+++ m4-apis/week1/1-catwalk-promises/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const STEP_INTERVAL_MS = 50;
+const STEP_SIZE_PX = 10;
+const DANCE_TIME_MS = 5000;
+const DANCING_CAT_URL =
+  'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+
+const walkingCatUrl = document.querySelector('img').src;
+
+function walk(img, startPos, stopPos) {
+  return new Promise(async (resolve) => {
+    let catPosition = startPos;
+
+    while (catPosition < stopPos) {
+      catPosition += STEP_SIZE_PX; //make a step forward
+      img.style.left = `${catPosition}px`;
+      await wait(STEP_INTERVAL_MS);
+    }
+
+    resolve();
+  });
+}
+
+function dance(img) {
+  return new Promise(async (resolve) => {
+    img.src = DANCING_CAT_URL;
+    await wait(DANCE_TIME_MS);
+    img.src = walkingCatUrl;
+    resolve();
+  });
+}
+
+function catWalk() {
+  const cat = document.querySelector('img');
+  const startPos = -cat.width;
+  const centerPos = (window.innerWidth - cat.width) / 2;
+  const stopPos = window.innerWidth;
+
+  walk(cat, startPos, centerPos)
+    .then(() => dance(cat))
+    .then(() => walk(cat, centerPos, stopPos))
+    .then(() => catWalk());
+}
+
+window.addEventListener('load', catWalk);
+
+function wait(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
