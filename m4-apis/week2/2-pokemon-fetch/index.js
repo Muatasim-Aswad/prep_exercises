de510a48a53543fedbb308df5fcd380b8fe941f3
Muatasim-Aswad <malasoad2021@gmail.com>
2024-09-05 22:52:59 +0200

    add api 2 prep exercise

---
+++ m4-apis/week2/2-pokemon-fetch/index.js
@@ -0,0 +1,53 @@
+'use strict';
+/*------------------------------------------------------------------------------
+ * In this exercise you will practice fetching data from a web API, using
+ * `fetch`, promises, async/await and try/catch.
+ *
+ * Your solution should both work for the "happy" path (using VALID_URL) as
+ * well handle the error in the "unhappy" path (when selecting INVALID_URL).
+ *
+ * You will need to decide which functions need to be made `async` and where
+ * `try/catch` blocks should be added.
+ *
+ * The HTML file already contains the necessary HTML elements.
+ *----------------------------------------------------------------------------*/
+
+const VALID_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=5';
+const INVALID_URL = 'https://pokeapi.co/api/v2/pokemons/?limit=5';
+
+async function fetchJSON(url) {
+  const response = await fetch(url);
+
+  if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+  const data = await response.json();
+  return data;
+}
+
+function renderResults(pokemons) {
+  const errorElement = document.querySelector('#error');
+  errorElement.innerText = '';
+
+  const pokemonsElement = document.querySelector('#json');
+  pokemonsElement.innerText = JSON.stringify(pokemons, null, 2);
+}
+
+function renderError(err) {
+  const pokemonsElement = document.querySelector('#json');
+  pokemonsElement.innerText = '';
+
+  const errorElement = document.querySelector('#error');
+  errorElement.innerText = err;
+}
+
+function main() {
+  const button = document.querySelector('#button');
+  button.addEventListener('click', () => {
+    const option = document.querySelector('#option');
+    const url = option.checked ? INVALID_URL : VALID_URL;
+
+    fetchJSON(url).then(renderResults).catch(renderError);
+  });
+}
+
+window.addEventListener('load', main);
